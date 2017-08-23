966fd3f9c54c17c353a81089a3f707f15fb08a06
1489568781 <1489568781@qq.com>
2017-08-23 12:36:11 +0800

    改变22

--- app/src/main/java/com/aojing/demo/test/MainActivity.java
+++ app/src/main/java/com/aojing/demo/test/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //嫦娥1222222
+        //嫦娥122222223423455
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
